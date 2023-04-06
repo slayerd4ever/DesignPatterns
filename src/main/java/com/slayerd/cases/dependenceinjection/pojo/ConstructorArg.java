2e1862ceb13515745c00e06880914f6dae87e7b6
slayerd <syd19970616@gmail.com>
2023-04-06 17:28:20 +0800

    UPDATE 构造器参数类更新:内部添加引用类型和非引用类型逻辑代码块

--- src/main/java/com/slayerd/cases/dependenceinjection/pojo/ConstructorArg.java
+++ src/main/java/com/slayerd/cases/dependenceinjection/pojo/ConstructorArg.java
@@ -2,16 +2,35 @@ package com.slayerd.cases.dependenceinjection.pojo;
 
 /**
  * 构造器注入参数
+ *
  * @author slayerd
  * @since 2023/4/6
  */
 public class ConstructorArg {
-    private boolean isRef;
+    private boolean isRef = false;
     private Class type;
     private Object arg;
 
     public boolean isRef() {
         return isRef;
+    }
+
+    public ConstructorArg(boolean isRef, Class type, Object arg) {
+        if (isRef){
+            if (type != null){
+                throw new IllegalArgumentException("当参数是引用类型的时候,type应为空");
+            }
+            if (!(arg instanceof String)) {
+                throw new IllegalArgumentException("当参数是引用类型的时候,arg应该指定引用BeanId");
+            }
+            this.isRef = isRef;
+        }else {
+            if (type == null || arg == null){
+                throw new IllegalArgumentException("当参数是非引用类型的时候,arg和type不能为空");
+            }
+            this.type = type;
+        }
+        this.arg = arg;
     }
 
     public void setRef(boolean ref) {
