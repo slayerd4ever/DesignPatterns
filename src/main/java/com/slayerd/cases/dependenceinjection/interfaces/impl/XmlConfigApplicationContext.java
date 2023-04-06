c332eb99cdc6111f4414fca71ce5b1c7dc038cef
slayerd <syd19970616@gmail.com>
2023-04-06 15:19:48 +0800

    ADD 新增对外暴露接口ApplicationContext的实现类,用于解析xml文件

---
+++ src/main/java/com/slayerd/cases/dependenceinjection/interfaces/impl/XmlConfigApplicationContext.java
@@ -0,0 +1,58 @@
+package com.slayerd.cases.dependenceinjection.interfaces.impl;
+
+import com.slayerd.cases.dependenceinjection.factory.BeanFactory;
+import com.slayerd.cases.dependenceinjection.interfaces.ApplicationContext;
+import com.slayerd.cases.dependenceinjection.pojo.BeanDefinition;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+/**
+ * 对外暴露的接口实现类
+ *
+ * @author slayerd
+ * @since 2023/4/6
+ */
+public class XmlConfigApplicationContext implements ApplicationContext {
+    private BeanFactory beanFactory;
+    private XmlBeanConfigParser parser;
+
+    public XmlConfigApplicationContext(String classPath) {
+        this.beanFactory = new BeanFactory();
+        this.parser = new XmlBeanConfigParser();
+        loadBeanDefinition(classPath);
+    }
+
+    /**
+     * 加载Bean元数据
+     *
+     * @param classPath xml文件路径
+     */
+    private void loadBeanDefinition(String classPath) {
+        InputStream inputStream = null;
+        try {
+            inputStream = this.getClass().getResourceAsStream("/" + classPath);
+            if (inputStream == null){
+                throw new RuntimeException("config can not found:" + classPath);
+            }
+            List<BeanDefinition> beanDefinitions = parser.parse(inputStream);
+        } finally {
+            if(inputStream != null){
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+        if (inputStream == null) {
+
+        }
+    }
+
+    @Override
+    public Object getBean(String beanId) {
+        return null;
+    }
+}
